b22364e286f7a4fffc281f0f7ea6fce47e5d7984
Eryn Todd <eryntodd55@gmail.com>
2026-04-24 21:59:37 -0400

    Add kernel implementation

---
+++ src/FilmCameraKernelImplementation.java
@@ -0,0 +1,14 @@
+/**
+ * Kernel implementation of FilmCamera.
+ * This version is for the AbstractFilmCamera.
+ */
+public class FilmCameraKernelImpl extends AbstractFilmCamera {
+
+    /**
+     * Constructs a new FilmCameraKernelImpl when there is no film loaded.
+     */
+    public FilmCameraKernelImpl() {
+        super();
+    }
+
+}
